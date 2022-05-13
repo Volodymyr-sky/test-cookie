c23cfa5ab33003fc0f97fc43e49180215fb58a55
Volodymyr <vovaguy@gmail.com>
2022-05-13 18:09:58 +0300

    update accordion and switch components

--- assets/js/init.cookie.plugin.js
+++ assets/js/init.cookie.plugin.js
@@ -1,11 +1,11 @@
 // import notification from "../../../json/notification.json" assert { type: "json" };
 // import modal from "../../../json/modal.json" assert { type: "json" };
 // import settings from "../../../json/cookie-settings.json" assert { type: "json" };
-// const parentSelector = document.getElementById('cookie-container');
+const parentSelector = document.getElementById('cookie-container');
 
 new CookiePlugin({
   // notification,
   // modal,
   // settings,
-  // parentSelector
+  parentSelector
 });
